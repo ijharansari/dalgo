5b1c6ed6d1e7fa077a223c532688758f0868db02
ijhar <ijhar.ansari@qtsolv.com>
2022-03-11 09:15:49 +0530

    Added Queue example

---
+++ array/QueueExample1.java
@@ -0,0 +1,9 @@
+/* 
+    Problem statement
+    Given an array and [lowVal, MaxVal]
+    Every element in array less than lowVal should appear before lowVal
+    Every element in array more than or equal to lowVal should appear after MaxVal
+*/
+public class QueueExample1 {
+    
+}
