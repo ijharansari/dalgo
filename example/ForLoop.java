25c6d51c595011ff895cef69e97287dfec777f70
ijharansari <ijharansari.education@gmail.com>
2022-03-30 18:11:00 +0530

    power sum

---
+++ example/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String args[]) {
+        int i=0;
+        for(i=0; i<=10; i++) {
+            System.out.print(i+ " ");
+        }
+        System.out.println("Outside for loop "+ i);
+    }
+}
--- example/PowerSum.java
+++ example/PowerSum.java
@@ -1,28 +1,42 @@
 /* 
     123 = 3^2+ 2^1+ 1^3 = 12;
+    1234
+    lastTwo = 34;
+    sum = sum + 4^3;
+
+    num = num/10;  123
+    12
+    1
+
 */
 
 public class PowerSum {
     public static void main(String [] args) {
-        int input = 0;
-        int temp = input%10;
-        input = input/10;
-        int lastNumber = temp, secondLastNumber = input%10;
+        int input = 22;
+        
+        if(Integer.toString(input).length() == 1) {
+            System.out.println(input);
+            return;
+        }
+
         int sum = 0;
-        sum = sum + (int)Math.pow(lastNumber, secondLastNumber);
-        for(int i = 0; i< Integer.toString(input).length()+1; i++) {
-            lastNumber = input%10;
-            input = input/10;
-            System.out.println("This is the inside "+ input);
-            if(input == 0) {
-                secondLastNumber = temp;
+        //check if the size of the digit is 2
+        if(Integer.toString(input).length() == 2) {
+            System.out.println((int)Math.pow(input%10,input/10));
+            return;
+        }
+
+        int temp = input%10;
+        int lastTwo = 0;
+        while(true) {
+            lastTwo = input%100;
+            if(Integer.toString(lastTwo).length() == 2) {
+                sum = sum + (int)Math.pow(lastTwo%10, lastTwo/10);
             }else {
-                secondLastNumber = input%10;
-            }
-            sum = sum + (int)Math.pow(lastNumber, secondLastNumber);
-            if(input == 0) {
+                sum = sum + (int)Math.pow(lastTwo,temp);
                 break;
             }
+            input = input/10;
         }
 
         System.out.println(sum);
