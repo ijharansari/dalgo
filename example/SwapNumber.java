e5bd3c008ed6f54b323f63f66d71e35c2692f2ba
ijharansari <ijharansari.education@gmail.com>
2022-03-31 09:35:27 +0530

    started swapping number logic

---
+++ example/SwapNumber.java
@@ -0,0 +1,16 @@
+/* 
+    AA BB CC DD E
+    int i=0; i+4;
+
+*/
+public class SwapNumber {
+    public static void main(String args[]) {
+        String words = "AABBCCDD";
+        if(words.length() < 4) {
+            System.out.println(words);
+            return;
+        }
+
+
+    }
+}
