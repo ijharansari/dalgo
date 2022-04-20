95f7688978bfb8e2eb70e026d8b286e840bf3da1
ijharansari <ijharansari.education@gmail.com>
2022-04-20 21:14:22 +0545

    Simple linked list

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,24 @@
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+    }
+    Node(Node next,int data) {
+        this.next = next;
+        this.data = data;
+    } 
+}
+public class LinkedList {
+    public static void main(String args[]) {
+        Node head = new Node(1);
+        Node node1 = new Node(head,2);
+        Node node2 = new Node(node1,3);
+
+        Node a = node2;
+        while(a != null) {
+            System.out.println(a.data);
+            a = a.next;
+        }
+    }
+}
